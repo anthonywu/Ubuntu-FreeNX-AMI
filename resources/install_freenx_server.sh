21585b9349b4b6abed82fd99fec680df33eb4bc1
Anthony Wu <wu@learnsprout.com>
2015-03-01 21:55:17 -0800

    NX install refinement

--- resources/install_freenx_server.sh
+++ resources/install_freenx_server.sh
@@ -15,7 +15,10 @@ sudo cp ./nxsetup /usr/lib/nx/nxsetup
 # Produces public/private dsa key pair artifacts at:
 # - /etc/nxserver/users.id_dsa
 # - /etc/nxserver/users.id_dsa.pub
-sudo /usr/lib/nx/nxsetup --install --setup-nomachine-key
+sudo /usr/lib/nx/nxsetup --install --setup-nomachine-key || true
+
+# Note: use `true` to force the above statement to be true because it exits error on this ignorable warning:
+# "Setting up cups nxipp backend ...cp: ‘/usr/lib/cups/backend/ipp’ and ‘/usr/lib/cups/backend/ipp’ are the same file"
 
 echo "Save this private key to your local machine:"
 sudo cat /etc/nxserver/users.id_dsa
@@ -24,6 +27,13 @@ sudo cat /etc/nxserver/users.id_dsa
 if ! grep "AllowUsers ubuntu nx" /etc/ssh/sshd_config; then
 	sudo sh -c 'echo "AllowUsers ubuntu nx" >> /etc/ssh/sshd_config'
 fi
+
+set +e
+
+sudo sed -i s/"PasswordAuthentication no"/"PasswordAuthentication yes"/g /etc/ssh/sshd_config
+
+grep PasswordAuthentication /etc/ssh/sshd_config
+
 sudo ssh-keygen -A
 /etc/init.d/ssh restart
 
